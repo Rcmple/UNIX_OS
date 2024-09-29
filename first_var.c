5b3c32e0107ac545431293f06843a2090c901991
Rcmple <imaginelipa@ImagineLipa.>
2024-09-29 20:10:37 +0300

    made HW2

---
+++ first_var.c
@@ -0,0 +1,41 @@
+//
+// Created by rcmpl on 29.09.2024.
+//
+
+#include <sys/stat.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int main() {
+    int fd[2];
+
+    if(pipe2(fd, O_NONBLOCK) == -1) {
+        perror("pipe2");
+        return 1;
+    }
+
+    int cnt_of_bytes = 0;
+    int stop = 0;
+
+    while(!stop) {
+        if(write(fd[1], "c", 1) > 0) {
+            cnt_of_bytes++;
+        } else {
+            stop = 1;
+        }
+    }
+
+    if(close(fd[0]) < 0) {
+        printf("Error with closing fd[0]\n");
+        exit(-1);
+    }
+
+    if(close(fd[1]) < 0) {
+        printf("Error with closing fd[1]\n");
+        exit(-1);
+    }
+
+    printf("Count of bytes: %d\n", cnt_of_bytes);
+}
---
+++ second_var.c
@@ -0,0 +1,33 @@
+//
+// Created by rcmpl on 29.09.2024.
+//
+
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+
+int main() {
+    int fd[2];
+
+    if(pipe(fd) == -1) {
+        printf("Cant open pipe\n");
+        exit(-1);
+    }
+
+    ssize_t sz = fcntl(fd[1], F_GETPIPE_SZ);
+
+    if(close(fd[0]) < 0) {
+        printf("Error with closing fd[0]\n");
+        exit(-1);
+    }
+
+    if(close(fd[1]) < 0) {
+        printf("Error with closing fd[1]\n");
+        exit(-1);
+    }
+
+    printf("Size of pipe: %zu\n", sz);
+}
